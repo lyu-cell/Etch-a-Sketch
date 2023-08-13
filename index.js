bbdaac4384aa99891c3696b2b5e22e1d99860a3e
lyu-cell <shizueizawa340@gmail.com>
2023-08-14 01:55:14 +0600

    new hover effect has been add, which leaves behind a pixalated trail just like a pen

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const grid = document.querySelector(".grid-size");
 
 // responsive grid related functions and loops
+const container = document.querySelector(".container")
 
 grid.addEventListener("click", () => {
     let userInput = prompt("Enter a Number Under 100");
@@ -8,17 +9,20 @@ grid.addEventListener("click", () => {
     if (userInput <= 100) {
 
         for (let i = 0; i < userInput; i++) {
-            const container = document.querySelector(".container")
+            
             const div = document.createElement("div");
             div.classList.add("column");
-            div.setAttribute("style", " display: flex; flex: 1; border: 1px solid green;");
+            div.setAttribute("style", " display: flex; flex: 1;");
             container.appendChild(div);
         
             for (let i = 0; i < userInput; i++) {
                 const squareDiv = document.createElement("div");
                 squareDiv.classList.add("div1")
-                squareDiv.setAttribute("style", "flex: 1; background-color: black; border: 1px solid white;");
+                squareDiv.setAttribute("style", "flex: 1; background-color: grey; ;");
                 div.appendChild(squareDiv)
+                squareDiv.addEventListener("mouseenter", () => {
+                    squareDiv.style.cssText = "flex: 1; background-color: white;";
+                });
             }
         }
     } 
@@ -30,7 +34,16 @@ grid.addEventListener("click", () => {
     else (
         alert("Enter Only Numbers")
     )
-
 });
 
-const div = document.querySelector()
+
+
+
+
+
+
+
+
+
+
+    
